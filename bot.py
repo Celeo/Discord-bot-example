13a0368c2c191796c85fb53607519d4c942c8c39
Celeo <celeodor@gmail.com>
2016-05-03 14:45:24 -0700

    Updates

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import discord
 from discord.ext import commands
 import json
 
@@ -21,24 +20,25 @@ async def on_error(event, *args, **kwargs):
 
 @bot.event
 async def on_message(message):
-    print('Messge from "{0.author.name}" of "{0.content}"'.format(message))
+    # print('Messge from "{0.author.name}" of "{0.content}"'.format(message))
     await bot.process_commands(message)
 
 
 @bot.event
 async def on_member_update(before, after):
-    print('{0.name} changed their profile'.format(after))
+    # print('{0.name} changed their profile'.format(after))
+    pass
 
 
 @bot.event
 async def on_typing(channel, user, when):
-    print('{1.name} is typing in channel {0.name}'.format(channel, user))
+    # print('{1.name} is typing in channel {0.name}'.format(channel, user))
+    pass
 
 
 @bot.command(name='slap', description='slap a user')
 async def slap(user):
-    print('slap command')
-    await bot.say('* slaps {} *'.format(user))
+    await bot.say('* slaps {} around a bit with a large trout*'.format(user))
 
 
 if __name__ == '__main__':
