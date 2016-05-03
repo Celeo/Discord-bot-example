434ad6a1ca14ff5b06211fe61b3015b496180df2
Celeo <celeodor@gmail.com>
2016-05-03 11:52:08 -0700

    First command is working

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+import discord
 from discord.ext import commands
 import json
 
@@ -10,6 +12,33 @@ bot = commands.Bot(command_prefix=config['COMMAND_PREFIX'], description=config['
 @bot.event
 async def on_ready():
     print('Running ...')
+
+
+@bot.event
+async def on_error(event, *args, **kwargs):
+    print('Error: ' + str(event))
+
+
+@bot.event
+async def on_message(message):
+    print('Messge from "{0.author.name}" of "{0.content}"'.format(message))
+    await bot.process_commands(message)
+
+
+@bot.event
+async def on_member_update(before, after):
+    print('{0.name} changed their profile'.format(after))
+
+
+@bot.event
+async def on_typing(channel, user, when):
+    print('{1.name} is typing in channel {0.name}'.format(channel, user))
+
+
+@bot.command(name='slap', description='slap a user')
+async def slap(user):
+    print('slap command')
+    await bot.say('* slaps {} *'.format(user))
 
 
 if __name__ == '__main__':
